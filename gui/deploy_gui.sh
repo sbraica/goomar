9e59c3e93c47463d41a5724f31accc228d998539
Stipe Braica <stipe.braica@internationalvalueservices.com>
2025-11-03 14:20:43 +0100

    gui build fix

--- gui/deploy_gui.sh
+++ gui/deploy_gui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
 echo "BUILD GUI"
-sed -i '' "s/^GIT_COMMIT=.*/GIT_COMMIT=$(git rev-parse --short HEAD)/" assets/.env
+sed -i "s/^GIT_COMMIT=.*/GIT_COMMIT=`git rev-parse --short HEAD`/" assets/.env
 dart run build_runner build --delete-conflicting-outputs
 flutter build web --no-tree-shake-icons --base-href "/"
 docker build -t goomar_gui .
