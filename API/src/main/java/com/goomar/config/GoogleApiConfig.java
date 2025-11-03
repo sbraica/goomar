89d1eb8fa433a81d2510dbaaadd90b7486129bcf
Stipe Braica <stipe.braica@internationalvalueservices.com>
2025-11-03 18:49:17 +0100

    gui build fix

--- API/src/main/java/com/goomar/config/GoogleApiConfig.java
+++ API/src/main/java/com/goomar/config/GoogleApiConfig.java
@@ -1,11 +1,11 @@
 package com.goomar.config;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
@@ -13,9 +13,16 @@ import com.google.api.services.calendar.Calendar;
 import com.google.api.services.gmail.Gmail;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,56 +32,74 @@ public class GoogleApiConfig {
     private static final String APPLICATION_NAME = "Goomar App";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
-
-    // Combined scopes for Calendar + Gmail
     private static final List<String> SCOPES = Arrays.asList(
             "https://mail.google.com/",
             "https://www.googleapis.com/auth/calendar"
     );
 
-    private Credential getCredentials() throws Exception {
-        var httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    /**
+     * Creates the GoogleAuthorizationCodeFlow bean.
+     */
+    @Bean
+    public GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow() throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
 
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
                 JSON_FACTORY,
                 new InputStreamReader(new ClassPathResource("credentials.json").getInputStream())
         );
 
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        return new GoogleAuthorizationCodeFlow.Builder(
                 httpTransport,
                 JSON_FACTORY,
                 clientSecrets,
                 SCOPES
         )
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
                 .build();
-
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder()
-                .setPort(8080)
-                .setCallbackPath("/Callback")
-                .build();
-
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
     @Bean
-    public Calendar googleCalendarService() throws Exception {
-        return new Calendar.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JSON_FACTORY,
-                getCredentials()
-        ).setApplicationName(APPLICATION_NAME).build();
-    }
-
-    @Bean
-    public Gmail googleGmailService() throws Exception {
+    public Gmail gmailService(GoogleAuthorizationCodeFlow flow, Environment env) throws Exception {
+        Credential credential = flow.loadCredential("user");
         return new Gmail.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 JSON_FACTORY,
-                getCredentials() // <-- same credential as Calendar
-        )
-                .setApplicationName(APPLICATION_NAME)
-                .build();
+                credential
+        ).setApplicationName(APPLICATION_NAME).build();
+    }
+
+    /**
+     * Creates the Calendar service using the same credential.
+     */
+    @Bean
+    public Calendar calendarService(GoogleAuthorizationCodeFlow flow, Environment env) throws Exception {
+        Credential credential = flow.loadCredential("user");
+        return new Calendar.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(),
+                JSON_FACTORY,
+                credential
+        ).setApplicationName(APPLICATION_NAME).build();
+    }
+
+    @RestController
+    public static class GoogleOAuthController {
+
+        private final GoogleAuthorizationCodeFlow flow;
+
+        public GoogleOAuthController(GoogleAuthorizationCodeFlow flow) {
+            this.flow = flow;
+        }
+
+        @GetMapping("/oauth2/callback")
+        public String callback(@RequestParam String code) throws IOException {
+            TokenResponse tokenResponse = flow.newTokenRequest(code)
+                    .setRedirectUri("https://terminapi.bosnic.hr/oauth2/callback") // Production URI
+                    .execute();
+
+            flow.createAndStoreCredential(tokenResponse, "user");
+            return "Authorization successful!";
+        }
     }
 }
